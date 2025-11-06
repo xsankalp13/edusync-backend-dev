4b6d7bd0c0315ce556977434f53145b1a8809f58
Singh <singh.ashishhhh@gmail.com>
2025-11-06 14:15:14 +0530

    Added: Added enums EducationLevel for Teacher

--- edusync/src/main/java/com/project/edusync/uis/model/enums/EducationLevel.java
+++ edusync/src/main/java/com/project/edusync/uis/model/enums/EducationLevel.java
@@ -1,4 +1,18 @@
 package com.project.edusync.uis.model.enums;
 
 public enum EducationLevel {
+    /**
+     * A Bachelor's degree.
+     */
+    BACHELORS,
+
+    /**
+     * A Master's degree.
+     */
+    MASTERS,
+
+    /**
+     * A Doctoral degree (Doctor of Philosophy).
+     */
+    PHD
 }
